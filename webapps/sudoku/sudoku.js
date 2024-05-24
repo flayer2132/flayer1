d1f89f47a072a496d902c168b4675c21ce6fa4ed
Dave <117615331+flayer2132@users.noreply.github.com>
2024-05-24 16:03:05 +0800

    Add files via upload

--- webapps/sudoku/sudoku.js
+++ webapps/sudoku/sudoku.js
@@ -1,10 +1,10 @@
 // init global variables
 let newSquare = "";
 let lastSquare = "";
-
-const chars = "abcdefghi";
 let locked = [];
 let win = [];
+
+const chars = "abcdefghi";
 
 let grid = {
   seta: [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -18,21 +18,27 @@ let grid = {
   seti: [0, 0, 0, 0, 0, 0, 0, 0, 0]
 };
 
+//used for dupicate checking in grid
 let check = Object.keys(grid);
 
 let games = 0;
 let errors = 0;
 
+window.onload = clearAll();
 
 //-------------------------------------------------------------------------------
+
+// TODO: add confirm prompt to solve and newgame
+// TODO: add solve() functionality
+
+// Mbug: does not check pre-generated rows for victory 
+// can generate full row of clues and therfore vicory never possible
 
 //mBUG: can generate impossible f columns (~5%) will restart generation on detection
 //mBUG: does not update existing highlighted numbers if they become legal
 
 //-------------------------------------------------------------------------------
 
-
-window.onload = clearAll();
 
 //check if entire column is correct
 function winV(set){
@@ -91,14 +97,44 @@ function victory(){
   };
 };
 
-//clear sets and grid text
-//clears edited styling
+
+//cylces though all cells in grid
+//checks if cell is empty
+function solve(){
+  //cycle though each set a - i
+    for (let c = 0; c < 9; c++) {
+      let set = chars[c];
+      let fullSet = "set" + set;
+      //cycle though each pos 0 - 8
+      for(i = 0; i < 9; i++){
+        let check = grid[fullSet][i];
+        //check if number > 0
+        if (check == 0){
+          //do a thing
+        };
+      };
+
+    };
+};
+
+
+//clear all sets
+//clears edited text and styling from board
+//reset all global variables
 function clearAll(){
 
-  //clear values of all sets
-  check.forEach(element => {
-    grid[element] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  });
+  //set all values to 0
+  grid = {
+    seta: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setb: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setc: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setd: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    sete: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setf: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setg: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    seth: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    seti: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  };
   
   //remove text from game board
   //remove styling from game board
@@ -111,45 +147,70 @@ function clearAll(){
       };
   };
 
-  //clear global variables
+  //reset global variables
   newSquare = "";
   lastSquare = "";
   locked = [];
   win = [];
 
-  //generate new grid
-  nextSet();
+  cycleGrid();
 
 };
 
-//cycle through all nine sets and run assign for each set
-function nextSet(){
 
-  let total = 0;
+//cycle through all sets and run assign for each position in each set
+//scrub out some numbers after generation
+function cycleGrid(){
 
-  const clues = 9;
-  const columns = 9;
+  let error = false;
+  let fails = 0;
 
-  let x = false;
-  
-  for (let i = 0; i < columns; i++) {
-    let set = chars[i];
-    console.log("ASSIGNING SET " + set);
-    total = total + clues;
-    x = assignClues(set, clues);
-    if (x == true){
+  let backup = {
+    seta: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setb: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setc: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setd: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    sete: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setf: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    setg: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    seth: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    seti: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  };
+
+  for (let i = 1; i < 10; i++) {
+    let set = chars[(i - 1)];
+    console.log("assigning set " + set);
+
+    //assign clues to each set, returning true on fail
+    error = assignClues(set);
+
+    //if cannot assign all 9 postions in set, re-assign entire grid
+    if (error == true){
+      fails++;
+      //start cycle again
       i = 0;
-      errors++;
+      //reset grid
+      grid = {
+        seta: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        setb: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        setc: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        setd: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        sete: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        setf: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        setg: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        seth: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        seti: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      };
+      console.log("** failed to generate set " + set + " -- re-generating entire grid **");
     };
   };
-  games++;
-  console.log("Games: " + games);
-  console.log("Generation Errors: " + errors);
+
+  console.log("ASSIGNMENT COMPLETE " + "generation restarts: " + fails);
+
   scrubGrid();
-  newSquare = "";
-  lastSquare = "";
   
 };
+
 
 //clear numbers from grid
 function scrubGrid(){
@@ -179,7 +240,11 @@ function scrubGrid(){
 
   };
 
+  newSquare = "";
+  lastSquare = "";
+
 };
+
 
 //slices out given index (p) from array (array)
 //returns updated array
@@ -199,15 +264,49 @@ function slice(p, array){
 
 };
 
-//assign a number of clues (clues) for the given set (set)
-function assignClues(set, clues){
+
+//check given set 
+//find unfilled positions
+//return a new array of positions where filled pos in checked set are removed
+function checkSet(set){
+
+  //find correct set and create local array from set
+  let temp = "set" + set;
+  let testSet = grid[temp];
+
+  let newSet = [];
+
+  let i = 0;
+  
+  //cycle though local array
+  testSet.forEach((element) => {
+    
+    //if pos is empty, add key to newSet
+    if(element == 0){
+      //push index of found 0 to new array
+      newSet.push(i);
+    };
+
+    i++;
+
+  });
+ 
+  return newSet;
+
+};
+
+
+//assign numbers for the given set (set)
+function assignClues(set){
+
+  //check existing set and assign to free
+  //skip any pos in free that already contains a number > 0
+  let free = checkSet(set);
 
   let pool = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let free = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   let backup = [];
 
   let i = 0;
-  let test = 0;
   let cycle = 0;
 
   let x = false;
@@ -223,7 +322,7 @@ function assignClues(set, clues){
     empty = Math.floor((Math.random() * free.length));
     pos = free[empty];
 
-    console.log("next position: " + (pos + 1));
+    // console.log("next position: " + (pos + 1));
 
     //slice empty position from free
     cut = slice(empty, free);
@@ -235,15 +334,13 @@ function assignClues(set, clues){
 
   //randomly assign a number to cells in current column for the given number of clues (clues) 
   //track number using counter (i)
-  while (i < clues){
+  while (i < 9){
 
     //select a random unused number to insert into pos
     select = Math.floor((Math.random() * pool.length));
     num = pool[select];
 
-    console.log("next number: " + num);
-    
-    console.log("checking validity of " + num + " in grid " + (pos+1) + set);
+    // console.log("checking validity of " + num + " in grid " + (pos + 1) + set);
 
     //check for duplicates horizontally
     x = checkH(pos, num);
@@ -253,8 +350,6 @@ function assignClues(set, clues){
     //if a dupicate is detected
     if (x == true || y == true){
 
-      console.log(true);
-
       //store failed numbers in backup array
       backup.push(num);
 
@@ -262,27 +357,26 @@ function assignClues(set, clues){
       cut = slice(select, pool);
       pool = cut;
 
-      console.log(pool);
-      console.log(backup);
+      // console.log(pool);
+      // console.log(backup);
 
       //impossible generaton detector
-      //break generation and restart grid
-      if(cycle > 20){
-        console.log("error: cycle maximum detected in set " + set + " " + free.length);
+      //break generation for current set if cannot assign after number of cycles
+      if(cycle > 30){
+        console.log("unable to assign set " + set);
         return true;
       };
 
       //if no numbers remain that fit into selected position, coloumn assignment must be invalid.
-      //erase column, reset counter and try to assign entire column again
+      //erase column, reset counter and re-assign entire column
       if(pool.length == 0){
-        console.log("Error: invalid set assignment - erasing set");
+        console.log("assignment invalid - erasing set");
         eraseColumn(set);
         i = 0;
-        test = 0;
         cycle++;
         backup = [];
         pool = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        free = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        free = checkSet(set);
         newPosition();
       };
 
@@ -308,26 +402,19 @@ function assignClues(set, clues){
       cut = slice(select, pool);
       pool = cut;
 
-      //move to next random cell (i), reset loop detection (test), pick a new random cell
+      //increment cells complted (i) and pick a new random cell
       i++;
-      test = 0;
       if (free.length > 0){
         newPosition();
       };
 
-      console.log(pool);
+      // console.log(pool);
 
-    };
-
-    //infininte loop detection and break
-    test++
-    if (test > 20){
-      alert("infinite loop detected");
-      console.log("ERROR: INF LOOP DETECTED - SKIPPING");
-      break;
     };
 
   };
+
+  return false;
 
 };
 
@@ -641,7 +728,7 @@ function clearError(a, p){
 function lockCell(set, pos){
   let lock = (pos + 1) + set;
   locked.push(lock);
-  console.log("locking " + lock);
+  // console.log("locking " + lock);
 };
 
 // set current and previous square variables
@@ -762,4 +849,9 @@ $(".erase").on("click", function(){
 //reload page when back is pressed
 $(".back").on("click", function(){
   window.location.reload();
+});
+
+//clear all and run a new game when new game is pressed
+$(".newgame").on("click", function(){
+  clearAll();
 });
